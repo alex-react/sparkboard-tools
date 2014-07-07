171466f175e7fb258b9568c3b92a90ced0a2bdaf
Matthew Huebert <no-email@gmail.com>
2014-07-07 19:43:38 -0400

    Remove reference to merge-firebase-rules

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ exports.SubscriptionMixin = require("./lib/react-subscription-mixin")
 exports.fetchSubscriptions = require("./lib/fetch-subscriptions")
 exports.utils = require("./lib/utils")
 exports.Router = require("./lib/router")
-exports.mergeFirebaseRules = require("./lib/merge-firebase-rules")
 
 exports.firebaseSubscription = require("./lib/firebase-subscription")
 exports.firebaseRelationalSubscription = require("./lib/firebase-relational-subscription")
